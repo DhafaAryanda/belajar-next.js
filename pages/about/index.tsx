e10c392ccd27b73aa34ab7db8646265f392b8f9c
dhofaaryanda4@gmail.com <dhofaaryanda4@gmail.com>
2025-02-17 18:16:54 +0700

    feat: tugas hari-6

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,5 +1,20 @@
+import Link from 'next/link'
 import React from 'react'
 
-export default function About() {
-  return <div>About Page</div>
+function about() {
+  return (
+    <div className=" ">
+      <div>
+        <h1 className="text-2xl font-bold">About Page</h1>
+        <Link
+          className="text-blue-500 underline font-semibold"
+          href="/about/me"
+        >
+          About Me
+        </Link>
+      </div>
+    </div>
+  )
 }
+
+export default about
---
+++ pages/api/user/[id].tsx
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  id?: string | string[] | undefined
+  name?: string
+  message?: string
+  data?: object
+  headers?: string | string[] | undefined
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  if (req.method === 'POST') {
+    res.status(200).json({
+      id: req.query.id,
+      name: 'John Doe',
+      data: req.body,
+      headers: req.headers['api-token'],
+    })
+  } else {
+    res.status(403).json({
+      message: 'Method not allowed',
+    })
+  }
+}
