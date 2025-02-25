29d95c2ca69dfc02eb0042a6d7c8742fcb73c8ec
dhofaaryanda4@gmail.com <dhofaaryanda4@gmail.com>
2025-02-26 01:04:25 +0700

    feat: tugas hari-12

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,20 +1,33 @@
+// import HeavyComponent from '@/components/HeavyComponents'
+import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import { useState } from 'react'
+import dynamic from 'next/dynamic'
 
-function about() {
+const HeavyComponent = dynamic(() => import('@/components/HeavyComponents'), {
+  ssr: false,
+  loading: () => <p>Loading...</p>,
+})
+
+function AboutPage() {
+  const [show, setShow] = useState(false)
   return (
-    <div className=" ">
-      <div>
-        <h1 className="text-2xl font-bold">About Page</h1>
-        <Link
-          className="text-blue-500 underline font-semibold"
-          href="/about/me"
-        >
-          About Me
-        </Link>
-      </div>
+    <div>
+      <h1 className="text-2xl font-bold">About Page</h1>
+      <Link className="text-blue-500 underline font-semibold" href="/about/me">
+        About Me
+      </Link>
+      <button
+        className="bg-blue-500 text-white px-3 py-1 rounded-md"
+        onClick={() => setShow(!show)}
+      >
+        Show
+      </button>
+      {show && <HeavyComponent />}
+
+      {/* <Image width={400} height={600} alt="img" src="/img1.jpg" priority /> */}
     </div>
   )
 }
 
-export default about
+export default AboutPage
